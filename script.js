370e8c817bd3cf401d89fcfbe2b2af7365e2eb07
Danil Merinov <danilmerinov9@gmail.com>
2025-03-05 23:11:57 -0500

    added contact section

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// Theme Toggle Script
-const themeToggle = document.getElementById('theme-toggle');
-const themeIcon = themeToggle.querySelector('i'); // Get the icon element
 
-// Check for saved theme in localStorage
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = themeToggle.querySelector('i'); 
+
+
 const savedTheme = localStorage.getItem('theme');
 if (savedTheme) {
   document.body.classList.add(savedTheme);
   updateButtonIcon();
 }
 
-// Toggle theme on button click
+
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
   updateButtonIcon();
 
-  // Save theme preference to localStorage
+  
   const currentTheme = document.body.classList.contains('dark-theme') ? 'dark-theme' : '';
   localStorage.setItem('theme', currentTheme);
 });
 
-// Update button icon based on current theme
+
 function updateButtonIcon() {
   if (document.body.classList.contains('dark-theme')) {
     themeIcon.classList.remove('fa-sun');
